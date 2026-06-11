061d2f78cfd4bff30e6ef2e9f7091bb12a3146b7
SLKingston <82053065+SLKingston@users.noreply.github.com>
2026-06-11 20:51:12 -0300

    Add files via upload
    Upload dos arquivos da tabela.

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'copa2026-v1';
+const ASSETS = [
+  './Copa_2026.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
